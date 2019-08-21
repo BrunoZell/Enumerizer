cbf05ba34dead7cfb97e483880ce96773662fb53
Bruno Zell <bruno.zzell@gmail.com>
2019-08-21 09:22:41 +0200

    use the existing struct as the enumerator

---
+++ src/BrunoZell.Enumerizer/StartToEndEnumerizer.Enumerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace BrunoZell.Enumerizer
+{
+    public partial struct StartToEndEnumerizer : IEnumerable<int>, IEnumerator<int>
+    {
+        public int Current => _current ?? 0;
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext() =>
+            _current.HasValue
+                ? (Start < End)
+                    ? (_current += Step) <= End
+                    : (_current -= Step) >= End
+                : true | (_current = Start) != 0;
+
+        public void Reset() =>
+            _current = null;
+
+        public void Dispose() { }
+
+        public IEnumerator<int> GetEnumerator() => this;
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- src/BrunoZell.Enumerizer/StartToEndEnumerizer.cs
+++ src/BrunoZell.Enumerizer/StartToEndEnumerizer.cs
@@ -1,28 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-namespace BrunoZell.Enumerizer
+﻿namespace BrunoZell.Enumerizer
 {
-    public readonly struct StartToEndEnumerizer : IEnumerable<int>
+    public partial struct StartToEndEnumerizer
     {
         public readonly int Start;
         public readonly int End;
         public readonly int Step;
+        private int? _current;
 
         public StartToEndEnumerizer(int start, int end, int step = 1)
         {
             Start = start;
             End = end;
             Step = step;
+            _current = null;
         }
 
         public static StartToEndEnumerizer operator |(StartToEndEnumerizer e, int s) =>
             new StartToEndEnumerizer(e.Start, e.End, s);
-
-        public IEnumerator<int> GetEnumerator() =>
-            new ValueEnumerator(Start, End, Step);
-
-        IEnumerator IEnumerable.GetEnumerator() =>
-            GetEnumerator();
     }
 }
