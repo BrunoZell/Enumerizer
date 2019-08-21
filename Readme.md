2044f38db03585141b117b0b45994e9ebe0806ac
Bruno Zell <bruno.zzell@gmail.com>
2019-08-21 09:46:10 +0200

    add usage examples

--- Readme.md
+++ Readme.md
@@ -2,6 +2,33 @@
 
 A mini utility that provides a declarative way to iterate integers:
 
+```csharp
+// Import Enumerizer in each file you want to use it
+using static Enumerizer;
+
+// 0 1 2 3 4 5 6 7 8 9
+foreach (int i in 0 <= i < 10)
+    Console.Write(i + " ");
+
+// 10 9 8 7 6 5 4 3 2 1 0
+foreach (int i in 10 >= i >= 0)
+    Console.Write(i + " ");
+
+// 10 8 6 4 2 0
+foreach (int i in 10 >= i >= 0 | 2)
+    Console.Write(i + " ");
+
+// -13 -10 -7 -4 -1 2 5 8
+foreach (int i in -14 < i <= 10 | 3)
+    Console.Write(i + " ");
 ```
 
+A usage with Linq is also possible:
+
+```csharp
+using System.Linq;
+using static Enumerizer;
+
+// [ -3, -2, -1, 0, 1, 2, 3 ]
+int[] it = (-3 <= i <= 3).ToArray();
 ```
--- sample/BrunoZell.Enumerizer.Sample/Program.cs
+++ sample/BrunoZell.Enumerizer.Sample/Program.cs
@@ -10,24 +10,28 @@ namespace BrunoZell.Enumerizer.Sample
             Console.WriteLine("0 <= i < 10");
             foreach (int i in 0 <= i < 10)
             {
+                // 0 1 2 3 4 5 6 7 8 9
                 Console.Write(i + " ");
             }
 
             Console.WriteLine("\n\n10 >= i >= 0");
             foreach (int i in 10 >= i >= 0)
             {
+                // 10 9 8 7 6 5 4 3 2 1 0
                 Console.Write(i + " ");
             }
 
             Console.WriteLine("\n\n10 >= i >= 0 | 2");
             foreach (int i in 10 >= i >= 0 | 2)
             {
+                // 10 8 6 4 2 0
                 Console.Write(i + " ");
             }
 
             Console.WriteLine("\n\n-14 < i <= 10 | 3");
             foreach (int i in -14 < i <= 10 | 3)
             {
+                // -13 -10 -7 -4 -1 2 5 8
                 Console.Write(i + " ");
             }
 
